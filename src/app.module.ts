b05e77110886446288262460357f1e09e5ffc650
danilobrm <danilo.rodrigues@pagglo.com>
2024-07-15 15:40:45 -0300

    ref: app module remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-// import { AuthController } from './modules/auth/application/controllers/auth/auth.controller';
-// import { CreateUserService } from './modules/user/application/services/create-user/user.service';
-// import { UserRepositoryService } from './modules/user/infrastructure/repositories/user-repository/user-repository.service';
 import { UserModule } from './modules/user/user.module';
 import { LocationModule } from './modules/location/location.module';
 import { AuthModule } from './modules/auth/auth.module';
